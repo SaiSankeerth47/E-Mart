a1a9f2f593b499f3e7002e7d96195ce648bd6a03
vamshigarega <70465597+vamshigarega@users.noreply.github.com>
2023-04-26 18:05:57 -0500

    Updating ClassUML Diagram and Tests.py

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import *
+
+class MyViewTest(TestCase):
+    
+    def setUp(self):
+        self.client = Client()
+        self.my_model = MyModel.objects.create(name='test')
+    
+    def test_my_view(self):
+        url = reverse('my_view')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'My View')
+        
+    def tearDown(self):
+        self.my_model.delete()
